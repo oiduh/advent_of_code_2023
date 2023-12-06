25dc89e82742c8658a30700c94908bbb26579184
oiduh <m.bialek.95@gmail.com>
2023-12-06 21:29:08 +0100

    day 5 incredibly dogshit solution

--- day_5/go/main.go
+++ day_5/go/main.go
@@ -29,6 +29,7 @@ type Full_Transformer struct {
     transformers []A_to_B_Transformer
 }
 
+
 func main() {
     path := os.Args[1]
     absPath, err := filepath.Abs(path)
@@ -84,6 +85,98 @@ func main() {
     fmt.Printf("Result 1: %d\n", min_val_1)
 
     // potential candidates
+    seeds_int_2 := []int{}
+    ranges := []int{}
+    seeds_int_3 := []int{}
+    for x := 0; x < len(seeds); x++ {
+        num, err := strconv.Atoi(seeds[x])
+        check(err)
+        if x % 2 == 0 {
+            seeds_int_2 = append(seeds_int_2, num)
+            seeds_int_3 = append(seeds_int_3, num)
+        } else {
+            ranges = append(ranges, num)
+        }
+    }
+    fmt.Println(len(seeds_int_2))
+    fmt.Println(seeds_int_2)
+    fmt.Println(ranges)
 
+    x := transformers.transformers[0].range_maps 
+    sort.Slice(x[:], func (i, j int) bool {
+        return x[i].src < x[j].src
+    })
+    fmt.Println(len(x))
+    for idx := 0; idx < len(seeds_int_2); idx++ {
+        seed := seeds_int_2[idx]
+        range_ := ranges[idx]
+        for _, y := range x {
+            if seed + range_ < y.src || seed > y.src + y.range_ {
+                continue
+            }
+            if seed < y.src {
+                if seed + range_ >= y.src {
+                    seeds_int_3 = append(seeds_int_3, y.src)
+                }
+                if seed + range_ >= y.src + y.range_ {
+                    seeds_int_3 = append(seeds_int_3, y.src + y.range_)
+                }
+                continue
+            }
+            if seed > y.src && seed < y.src + y.range_ {
+                if seed + range_ >= y.src + y.range_ {
+                    seeds_int_3 = append(seeds_int_3, y.src + y.range_)
+                }
+            }
+        }
+    }
+    fmt.Println(len(seeds_int_3))
+
+    min_val_2 := math.MaxInt
+    for idx := 0; idx < len(seeds_int_3); idx++ {
+        for _, transformer := range transformers.transformers {
+            for _, mapper := range transformer.range_maps {
+                if mapper.src <= seeds_int_3[idx] && seeds_int_3[idx] < mapper.src + mapper.range_ {
+                    seeds_int_3[idx] = mapper.dst
+                    break
+                }
+            }
+        }
+        if seeds_int_3[idx] < min_val_2 {
+            min_val_2 = seeds_int_3[idx]
+        }
+    }
+    fmt.Printf("Result 2: %d\n", min_val_2)
+
+
+    h := transformers.transformers[len(transformers.transformers)-1].range_maps 
+    sort.Slice(h[:], func (i, j int) bool {
+        return h[i].dst < h[j].dst
+    })
+    fmt.Println(h[0].dst)
+    fmt.Println(h[0].range_)
+
+    a := h[0].dst
+    b := h[0].dst + h[0].range_ - 1
+    arr := []int{a, b}
+    for idy := 0; idy < 2; idy++ {
+        for idx := len(transformers.transformers) - 1; idx >= 0; idx-- {
+            for _, mapper := range transformers.transformers[idx].range_maps {
+                if mapper.dst <= arr[idy] && arr[idy] < mapper.dst + mapper.range_ {
+                    new_val := arr[idy] - mapper.dst + mapper.src
+                    arr[idy] = new_val
+                    break
+                }
+            }
+        }
+    }
+
+    for idx := 0; idx < len(seeds_int_2); idx++ {
+        if arr[0] >= seeds_int_2[idx] && arr[0] < seeds_int_2[idx] + ranges[idx] {
+            fmt.Println("the lowest destination can be found")
+        }
+    }
+    
+    fmt.Println(arr)
 
 }
